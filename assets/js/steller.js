13a7437ab7c14856e3c6a727dea5bd27c3d801a9
mohammad <MohammadAli.Dev62@gmail.com>
2025-05-28 19:54:57 +0500

    scroll

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -40,3 +40,22 @@ window.addEventListener('scroll', () => {
 		}
 	});
 });
+// Get the button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top, show the button
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// When clicked, scroll to the top
+scrollToTopBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // Smooth scrolling
+  });
+});
